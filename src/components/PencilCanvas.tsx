b9cfdb32a9f7b286f5f43ce1d73ef365e7987f7d
cr4l <carlfiler@me.com>
2026-04-09 20:42:01 -0700

    fix: constrain cursor image to 52x52 to prevent stretching

--- src/components/PencilCanvas.tsx
+++ src/components/PencilCanvas.tsx
@@ -283,7 +283,7 @@ export default function PencilCanvas({ onClearRef, onUndoRef, onRedoRef, onCount
         className="fixed inset-0 z-0"
         style={{ pointerEvents: disabled ? 'none' : 'auto' }}
       />
-      <div ref={cursorRef} id="pencil-cursor"><img src="/images/cursor-pencil.svg" alt="" draggable={false} /></div>
+      <div ref={cursorRef} id="pencil-cursor"><img src="/images/cursor-pencil.svg" alt="" draggable={false} style={{ width: 52, height: 52, objectFit: 'contain' }} /></div>
       <div
         ref={hintRef}
         id="pencil-hint"
